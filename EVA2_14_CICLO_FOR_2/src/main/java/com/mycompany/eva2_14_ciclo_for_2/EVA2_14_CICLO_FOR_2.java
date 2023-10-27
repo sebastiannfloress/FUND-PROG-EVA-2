9d9fed9188303561a041a628deec6d52106d2bb2
sebastiannfloress <143568236+sebastiannfloress@users.noreply.github.com>
2023-10-26 19:32:19 -0700

    Add files via upload

---
+++ EVA2_14_CICLO_FOR_2/src/main/java/com/mycompany/eva2_14_ciclo_for_2/EVA2_14_CICLO_FOR_2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_14_ciclo_for_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author saulf
+ */
+public class EVA2_14_CICLO_FOR_2 {
+
+    public static void main(String[] args) {
+        int noCalifas;
+        Scanner input = new Scanner (System.in);
+        System.out.println("Introduce el numero de calieficaciones");
+        noCalifas = input.nextInt();
+        for (int i = 0; i < noCalifas; i++){
+            System.out.println("Introduce la calificacion");
+            int califa = input.nextInt();
+            
+           
+            
+        }
+        
+        
+                
+    }
+}
---
+++ EVA_13_CICLO_FOR/src/main/java/com/mycompany/eva_13_ciclo_for/EVA_13_CICLO_FOR.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_13_ciclo_for;
+
+/**
+ *
+ * @author saulf
+ */
+public class EVA_13_CICLO_FOR {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("----------------------------------------------------");
+        for(int i = 9; i >= 0; i--) {
+            System.out.println(i);
+            
+            }
+        for (int i = 0; i <= 100; i+=2) {
+            System.out.print(i + "-");
+            
+            
+        }
+        
+        
+        
+        
+        
+    }
+}
